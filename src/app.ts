f10f5fae0367609c650fb5dbf8b666613ad29000
ShayeGun <shayegan.7200@gmail.com>
2023-08-05 13:36:22 +0330

    token for prod

--- src/app.ts
+++ src/app.ts
@@ -39,36 +39,36 @@ app.use('/api', limiter);
 
 // // FIX: for production only
 // check validity of token with each request
-// app.use(catchAsync(async (req, res, next) => {
-//     await token.checkValidity()
-//     console.log(token.getToken());
+app.use(catchAsync(async (req, res, next) => {
+    await token.checkValidity()
+    // console.log(token.getToken());
 
-//     req.token = token;
-//     next()
-// }))
+    req.token = token;
+    next()
+}))
 
 // FIX: for development only
-app.get('/api/shayrad/v1/get-token', async (req, res, next) => {
-    await token.checkValidity()
-    console.log(token.getToken())
-    res.json(token.getToken())
-})
+// app.get('/api/shayrad/v1/get-token', async (req, res, next) => {
+//     await token.checkValidity()
+//     console.log(token.getToken())
+//     res.json(token.getToken())
+// })
 
-app.use((req, res, next) => {
-    req.token = {
-        getToken() {
-            return {
-                "accessToken": "eyJhbGciOiJSUzI1NiIsImtpZCI6IjgyNUNFNDQ5OEU3MzQyNEJEMTlEOUY3OUQ3NEIyOEFEIiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE2OTA5NjM1NDAsImV4cCI6MTY5MDk2NzE0MCwiaXNzIjoiaHR0cHM6Ly9hdXRoLnNoYXlyYWQuaXIiLCJjbGllbnRfaWQiOiJzaC1uYWctcGFrbGVhbiIsImlhdCI6MTY5MDk2MzU0MCwic2NvcGUiOlsibmFqaV9hcGlfc2NvcGUiXX0.rRwK3Mk2248CKmVVLIzywOoUlEdTsfysx98ppKaBJJdI-p_KkVrO-kQsU4m7g9tqaM2msUtsTlEWgnll61_h99pcasDOguwMWPRqj8gSM7qKzqdCI1ZBRdJIBnUPtjTN6V3peMl-whewuXAUcFNrM7PCgkesDVsNasqvJKGGOR7k6xpTY3HCCuQf70SApQzpDzr7N4gCU_7JgBvxqHL3ozkcSYpMIM0Sgob1eBpbqD4m5UR4tt5W7_SRJJLABDyx0aJgGFxh1mS1M4bzjn_LKdi4zxK93w1hQEe6ICXiILPxkabK2kuCnYY-7uF74_sq5H6hkPJmmCmLXtTLk6cqBg",
-                "tokenType": "Bearer",
-                "scope": "naji_api_scope",
-                "createdAt": 1690963511923,
-                "expiresIn": 3600000
-            }
-        }
+// app.use((req, res, next) => {
+//     req.token = {
+//         getToken() {
+//             return {
+//                 "accessToken": "eyJhbGciOiJSUzI1NiIsImtpZCI6IjgyNUNFNDQ5OEU3MzQyNEJEMTlEOUY3OUQ3NEIyOEFEIiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE2OTA5NjM1NDAsImV4cCI6MTY5MDk2NzE0MCwiaXNzIjoiaHR0cHM6Ly9hdXRoLnNoYXlyYWQuaXIiLCJjbGllbnRfaWQiOiJzaC1uYWctcGFrbGVhbiIsImlhdCI6MTY5MDk2MzU0MCwic2NvcGUiOlsibmFqaV9hcGlfc2NvcGUiXX0.rRwK3Mk2248CKmVVLIzywOoUlEdTsfysx98ppKaBJJdI-p_KkVrO-kQsU4m7g9tqaM2msUtsTlEWgnll61_h99pcasDOguwMWPRqj8gSM7qKzqdCI1ZBRdJIBnUPtjTN6V3peMl-whewuXAUcFNrM7PCgkesDVsNasqvJKGGOR7k6xpTY3HCCuQf70SApQzpDzr7N4gCU_7JgBvxqHL3ozkcSYpMIM0Sgob1eBpbqD4m5UR4tt5W7_SRJJLABDyx0aJgGFxh1mS1M4bzjn_LKdi4zxK93w1hQEe6ICXiILPxkabK2kuCnYY-7uF74_sq5H6hkPJmmCmLXtTLk6cqBg",
+//                 "tokenType": "Bearer",
+//                 "scope": "naji_api_scope",
+//                 "createdAt": 1690963511923,
+//                 "expiresIn": 3600000
+//             }
+//         }
 
-    }
-    next()
-})
+//     }
+//     next()
+// })
 
 const options = yaml.load(fs.readFileSync(`${__dirname}/../swagger.yaml`, 'utf8'));
 
